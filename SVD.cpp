8664b49a77c65c5f880cb2e093a586d9319198cd
mathukumillia <mathukumillia@s.dcsdk12.org>
2017-04-19 21:27:45 -0700

    Single epoch works, is slow

--- SVD.cpp
+++ SVD.cpp
@@ -21,13 +21,18 @@ double **movieValues;
 
 double predictRating(int user, int movie)
 {
-  return inner_product(userValues[user], userValues[user] + (int)K, movieValues[movie], 0.0);
+  double rating = 0;
+  for (int i = 0; i < K; i++)
+  {
+    rating += userValues[user][i] * movieValues[movie][i];
+  }
+  return rating;
 }
 
 
 double error ()
 {
-
+  cout << "Calculating Validation error..." << "\n";
   fstream inputFile ("../um/all.dta");
   fstream indexFile ("../um/all.idx");
   double inputs [4] = {};
@@ -36,15 +41,14 @@ double error ()
   int counter = 0;
   double error = 0;
   double diff;
-  string index;
+  double index;
   if (inputFile.is_open() && indexFile.is_open())
   {
     while (inputFile >> inputs[0] >> inputs[1] >> inputs[2] >> inputs[3])
     {
-      getline(indexFile, index);
-      if (atoi(index.c_str()) == 1)
+      indexFile >> index;
+      if (index == 2)
       {
-        cout << "Getting error for point " << counter << "\n";
         diff = inputs[2] - predictRating(inputs[0], inputs[1]);
         error += diff * diff;
       }
@@ -61,6 +65,7 @@ void train(int user, int movie, int rating)
   // calculate the error with the current feature values
 	double err = lrate * ((double) rating - predictRating(user, movie));
 
+  // updates the movie and user vectors feature by feature
   double *uv = userValues[user];
   for (int i = 0; i < K; i++){
     userValues[user][i] += err * movieValues[movie][i];
@@ -71,30 +76,38 @@ void train(int user, int movie, int rating)
 void initialize()
 {
   // allocate and initialize the userValues and movieValues matrices
-  userValues = new double*[numUsers];
-  for (int i = 0; i < numUsers; i++)
+  // all of the +1 terms result from the fact that these arrays are 1
+  // indexed in the data
+  userValues = new double*[((int) numUsers + 1)];
+  for (int i = 0; i < numUsers + 1; i++)
   {
-    userValues[i] = new double[K];
-    fill(&userValues[0][0], &userValues[0][0] + sizeof(userValues), 0.1);
+    userValues[i] = new double[(int) K];
+    for (int j = 0; j < K; j++)
+    {
+      userValues[i][j] = 0.1;
+    }
   }
 
-  movieValues = new double*[numMovies];
-  for (int i = 0; i < numMovies; i++)
+  movieValues = new double*[((int) numMovies + 1)];
+  for (int i = 0; i < numMovies + 1; i++)
   {
-    movieValues[i] = new double[K];
-    fill(&movieValues[0][0], &movieValues[0][0] + sizeof(movieValues), 0.1);
+    movieValues[i] = new double[(int) K];
+    for (int j = 0; j < K; j++)
+    {
+      movieValues[i][j] = 0.1;
+    }
   }
 }
 
 void cleanUp()
 {
   // de-allocate user values and movie values to prevent memory leak
-  for (int i = 0; i < numUsers; i++)
+  for (int i = 0; i < numUsers + 1; i++)
   {
     delete [] userValues[i];
   }
   delete [] userValues;
-  for (int i = 0; i < numMovies; i++)
+  for (int i = 0; i < numMovies + 1; i++)
   {
     delete [] movieValues[i];
   }
@@ -103,6 +116,7 @@ void cleanUp()
 
 void runEpoch ()
 {
+  cout << "Running Epoch..." << "\n";
   // read in the training data and train on each input
   fstream inputFile ("../um/all.dta");
   fstream indexFile ("../um/all.idx");
@@ -110,18 +124,24 @@ void runEpoch ()
 
   //  counter keeps track of the number of points we've been through
   int counter = 0;
-  string index;
+  double index;
   if (inputFile.is_open() && indexFile.is_open())
   {
     while (inputFile >> inputs[0] >> inputs[1] >> inputs[2] >> inputs[3])
     {
-      getline(indexFile, index);
-      if (atoi(index.c_str()) == 1)
+      indexFile >> index;
+      // if the point in question is part of the training set, train on it
+      if (index == 1)
       {
-        cout << "Processing point " << counter << "\n";
         train(inputs[0], inputs[1], inputs[3]);
       }
       counter++;
+      // print the counter every 1000000 points; I don't print every point
+      // because that slows it down
+      if (counter % 1000000 == 0)
+      {
+        cout << counter << "\n";
+      }
     }
   }
   inputFile.close();
@@ -141,7 +161,6 @@ int main()
   // NOTE: Uncomment the while loop once we've optimized the single epoch stuff
   // while (initialError/finalError > 0.01) {
     initialError = finalError;
-    cout << "Running Epoch..." << "\n";
     runEpoch();
     finalError = error();
     cout << "Initial Error is: " << initialError << "\n";
