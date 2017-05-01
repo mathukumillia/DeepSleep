762285636f9007b9522639f1fe33a28e5097b95c
mathukumillia <anirudh.mathukumilli@gmail.com>
2017-04-30 17:09:48 -0700

    got SVD++ running

--- SVD++.cpp
+++ SVD++.cpp
@@ -7,6 +7,9 @@
 // a point looks like (user, movie, date, rating)
 #define POINT_SIZE 4 // the size of a single input point in the training data
 #define STOPPING_CONDITION 0.0001
+#define MAX_NEIGHBOR_SIZE 3341
+#define GAMMA_2 0.007 // obtained from the SVD++ paper
+#define LAMBDA_7 0.015 // obtained from the SVD++ paper
 
 using namespace std;
 
@@ -38,13 +41,16 @@ double *indices;
 
 // stores each user's neighborhoods
 // functionally, this is a 2D array that stores for each user the id of the
+// this is an int array because the double array is too large to work with
 // movies they provided feedback for
-double *neighborhoods;
+int *neighborhoods;
+int neighborhoods_size = (int) ((num_users + 1) * MAX_NEIGHBOR_SIZE);
+double *neighborhood_sizes;
 
 // y is a 2D array that holds K features for each of the movies
 // the plus one in the size derives from the fact that the movies are 1 indexed
 double *y;
-int y_size = (int) ((num_movies + 1) * K)
+int y_size = (int) ((num_movies + 1) * K);
 
 /*
 * Allocates memory and initializes user, movie, ratings, and indices arrays
@@ -59,18 +65,24 @@ void initialize()
 	user_values = new double[user_values_size];
 	for (int i = 0; i < user_values_size; i++)
 	{
-		user_values[i] = 0.1;
+		user_values[i] = 0.1; // arbitrary initial condition
 	}
 
 	movie_values = new double[movie_values_size];
+	y = new double[y_size];
 	for (int i = 0; i < movie_values_size; i++)
 	{
 		movie_values[i] = 0.1;
+		y[i] = 0.1; // this was an arbitrary initial condition
 	}
 
 	// create  the arrays that store the ratings input data and the indexes
 	ratings = new double[ratings_size];
 	indices = new double[((int) num_pts)];
+
+	neighborhoods = new int[neighborhoods_size];
+	neighborhood_sizes = new double[(int)(num_users + 1)];
+
 
 	cout << "Done allocating memory.\n";
 }
@@ -85,6 +97,9 @@ void clean_up()
 	delete [] movie_values;
 	delete [] ratings;
 	delete [] indices;
+	delete [] neighborhoods;
+	delete [] neighborhood_sizes;
+	delete [] y;
 }
 
 /*
@@ -102,15 +117,80 @@ void read_data()
 	fstream indices_file("../indices.bin", ios::in | ios::binary);
 	indices_file.read(reinterpret_cast<char *>(indices), sizeof(double) * num_pts);
 	indices_file.close();
+
+	// read in neighborhod data
+	fstream neighborhood_file("../neighborhoods.bin", ios::in | ios::binary);
+	neighborhood_file.read(reinterpret_cast<char *>(neighborhoods), sizeof(int) * (num_users + 1) * MAX_NEIGHBOR_SIZE);
+	neighborhood_file.close();
+
+	// read in the neighborhood size data
+	fstream nsize_file ("../neighborhood_sizes.bin", ios::in | ios::binary);
+	nsize_file.read(reinterpret_cast<char *>(neighborhood_sizes), sizeof(double) * (num_users + 1));
+	nsize_file.close();
 }
 
 /*
-* Predicts a rating given a user and a movie
+* Calculate the vector combining user factors and neighborhood factors
+* this is p_u + |N(u)|^(-1/2) * sum of y's in neighborhood of u
+* when using this, remember to delete the allocated memory for the user vector
 */
-double predict_rating(double user, double movie)
+double* get_user_vector(int user, int movie)
 {
-	/* TODO */
-	return 0.0;
+	// stores the sum of user vectors
+	// (all the y's in the users neighborhood plus the user factors)
+	// initializes to 0 because it accumulates the other values
+	double * user_vector_sum = new double[(int)K]();
+	// stores the movie for which we are obtaining y
+	int neighborhood_movie;
+	// loop through neighborhood and get sum of y vectors for each movie in 
+	// neighborhood
+	if(neighborhood_sizes[user] > 0)
+	{
+		for (int i = 0; i < neighborhood_sizes[user]; i++)
+		{
+			neighborhood_movie = neighborhoods[user * MAX_NEIGHBOR_SIZE + i];
+			// add the current neighborhood movies y vector to the user vector sum
+			for (int j = 0; j < K; j++)
+			{
+				user_vector_sum[j] += y[neighborhood_movie * (int)K + j];
+			}
+		}
+	}
+	// add the user factors to the neighborhood sum
+	// neighborhood sum must be divided by square root of neighborhood size 
+	// first
+	// then takes inner product of user vector and movie vector
+	if (neighborhood_sizes[user] > 0)
+	{
+		for (int i = 0; i < K; i++)
+		{
+			user_vector_sum[i] = user_vector_sum[i]/sqrt(neighborhood_sizes[user]) + user_values[user * (int)K + i];
+		}
+	}
+	else
+	{
+		for (int i = 0; i < K; i++)
+		{
+			user_vector_sum[i] = user_values[user * (int)K + i];
+		}
+	}
+	return user_vector_sum;
+}
+
+
+/*
+* Finds the predicted rating using the movie and the user vector sum that 
+* we found earlier
+*
+*/
+double predict_rating(int movie, double * user_vector_sum)
+{
+	double rating = 0;
+	for (int i = 0; i < K; i++)
+	{
+		rating += user_vector_sum[i] * movie_values[movie * (int)K + i];
+	}
+	return rating;
 }
 
 /*
@@ -130,7 +210,8 @@ double error(int set)
 		index = indices[i];
 
 		if (index == set) {
-			diff = ratings[i * POINT_SIZE + 3] - predict_rating(ratings[i + POINT_SIZE], ratings[i * POINT_SIZE + 1]);
+			double *user_vector_sum = get_user_vector((int)ratings[i + POINT_SIZE], (int)ratings[i * POINT_SIZE + 1]);
+			diff = ratings[i * POINT_SIZE + 3] - predict_rating((int)ratings[i * POINT_SIZE + 1], user_vector_sum);
 			error += diff * diff;
 			points_in_set += 1;
 		}
@@ -146,7 +227,42 @@ double error(int set)
 */
 void train(double user, double movie, double date, double rating)
 {
-	/* TODO */
+	double * user_vector_sum = get_user_vector((int)user, (int)movie);
+	double point_error = rating - predict_rating((int)movie, user_vector_sum);
+
+	// update the movie and user factors 
+	double movie_factor;
+	double user_factor;
+	for (int i = 0; i < K; i++)
+	{
+		// stores the current factor that we are updating
+		movie_factor = movie_values[(int)movie * (int)K + i];
+		// update the movie factor in the movie values array
+		movie_values[(int)movie * (int)K + i] = movie_factor + GAMMA_2 * (point_error * user_vector_sum[i] - LAMBDA_7 * movie_factor);
+		// stores the current user factor
+		user_factor = user_values[(int)user * (int)K + i];
+		// update the user factor in the user values array
+		user_values[(int)user * (int)K + i] = user_factor + GAMMA_2 * (point_error * movie_factor - LAMBDA_7 * user_factor);
+	}
+
+	// update the neighbors factors
+	double y_factor;
+	int movie_neighbor;
+	double size = neighborhood_sizes[(int)user];
+	for (int i = 0; i < size; i++)
+	{
+		movie_neighbor = neighborhoods[(int)user * MAX_NEIGHBOR_SIZE + i];
+		for(int j = 0; j < K; j++)
+		{
+			y_factor = y[movie_neighbor * (int)K + j];
+			movie_factor = movie_values[(int)movie * (int)K + j];
+			y[movie_neighbor * (int)K + j] = y_factor + GAMMA_2 * (point_error/sqrt(size) * movie_factor - LAMBDA_7 * y_factor);
+		}
+	}
+
+	// delete the user_vector_sum because it was allocated in the get_user_vector
+	// function
+	delete [] user_vector_sum;
 }
 
 /*
@@ -172,10 +288,10 @@ int main()
 	read_data();
 
 	double initialError = 10;
-	double finalError = error(2); // gets the initial validation error
+	double finalError = error(2); // gets the validation error before training
 	int counter = 0;
 
-	cout << "Initial Error is: " << initialError << "\n";
+	cout << "The starting error is: " << finalError << "\n";
 	while (initialError - finalError > STOPPING_CONDITION) {
 		cout << "Starting Epoch " << counter << "\n";
 		counter++;
---
+++ getNeighborhoods.cpp
@@ -0,0 +1,129 @@
+/*
+* gets the neighborhood of each user, stores it in binary file
+*
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <numeric>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+// a point looks like (user, movie, date, rating)
+#define POINT_SIZE 4 // the size of a single input point in the training data
+#define MAX_NEIGHBOR_SIZE 3341 // the largest any possible neighborhood can be
+
+// users and movies are one indexed
+const double num_users = 458293;
+const double num_pts = 102416306;
+
+// 2D array that stores the neighborhood of each user 
+// the neighborhood is just all of the movies for which the user has given
+// a rating
+// I use a 1D array to make it more time efficient
+int *neighborhoods;
+int neighborhoods_size = (int) ((num_users + 1) * MAX_NEIGHBOR_SIZE);
+// array stores how many movies are in the neighborhood of each user
+// essentially store |N(u)|
+double *neighborhood_sizes;
+
+// the arrays to store the ratings and indices data
+// note that ratings will be used as a 2D array as well
+double *ratings;
+int ratings_size = (int) (num_pts * POINT_SIZE);
+double *indices;
+
+void initialize ()
+{
+	cout << "Initializing.\n";
+	// create the arrays that store the ratings input data and the indexes
+	ratings = new double[ratings_size];
+	indices = new double[((int) num_pts)];
+	neighborhoods = new int[neighborhoods_size];
+	neighborhood_sizes = new double[(int)(num_users + 1)];
+	// initializes all of the sizes to 0
+	for (int i = 0; i < num_users + 1; i++)
+	{
+		neighborhood_sizes[i] = 0;
+	}
+}
+
+/*
+* Reads the input data into ratings and indices
+*/
+void read_data()
+{
+	cout << "Reading in training data.\n";
+	// read in ratings data
+	fstream ratings_file("../ratings.bin", ios::in | ios::binary);
+	ratings_file.read(reinterpret_cast<char *>(ratings), sizeof(double) * num_pts * POINT_SIZE);
+	ratings_file.close();
+
+	// read in index data
+	fstream indices_file("../indices.bin", ios::in | ios::binary);
+	indices_file.read(reinterpret_cast<char *>(indices), sizeof(double) * num_pts);
+	indices_file.close();
+}
+
+/*
+*
+* Read through all of the points and assemble the neighborhoods of each
+* user
+*/
+void assemble_neighborhoods()
+{
+	cout << "Assembling neighborhoods.\n";
+	double index;
+	double user;
+	double movie;
+	for (int i = 0; i < num_pts; i++)
+	{
+		index = indices[i];
+		// currently only assembles neighborhoods from the training set 1
+		if (index == 1)
+		{
+			user = ratings[i * POINT_SIZE];
+			movie = ratings[i * POINT_SIZE + 1];
+			neighborhoods[(int)user * MAX_NEIGHBOR_SIZE + (int)neighborhood_sizes[(int)user]] = (int)movie;
+			neighborhood_sizes[(int)user] += 1;
+		}
+	}
+}
+
+/*
+* Writes the neighborhoods and neighborhood sizes to binary files
+*
+*/
+void write_neighborhoods()
+{
+	cout << "Writing to binary.\n";
+	fstream neighborhoods_file ("neighborhoods.bin", ios::out | ios::binary);
+	fstream neighborhood_sizes_file ("neighborhood_sizes.bin", ios::out | ios::binary);
+	neighborhoods_file.write(reinterpret_cast<char *>(neighborhoods), sizeof(double) * (num_users + 1) * MAX_NEIGHBOR_SIZE);
+	neighborhood_sizes_file.write(reinterpret_cast<char *>(neighborhood_sizes), sizeof(double) * (num_users + 1));
+	neighborhoods_file.close();
+	neighborhood_sizes_file.close();
+}
+
+/*
+* Deallocate allocated memory
+*/
+void clean_up()
+{
+	delete [] ratings;
+	delete [] indices;
+	delete [] neighborhoods;
+	delete [] neighborhood_sizes;
+}
+
+int main()
+{
+	initialize();
+	read_data();
+	assemble_neighborhoods();
+	write_neighborhoods();
+	clean_up();
+	return 0;
+}
