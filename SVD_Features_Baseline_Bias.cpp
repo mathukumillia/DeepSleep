4abf1cdb614dfd8047bc9112fd7b0549056edf48
Vaishnavi Shrivastava <vshrivas@caltech.edu>
2017-05-06 19:49:04 -0700

    Optimized SVD Bias

--- SVD_Features_Baseline_Bias.cpp
+++ SVD_Features_Baseline_Bias.cpp
@@ -8,36 +8,47 @@
 #include "baselinePrediction.h"
 using namespace std;
 
+// a point looks like (user, movie, date, rating)
+#define POINT_SIZE 4 // the size of a single input point in the training data
 #define STOPPING_CONDITION 0
-#define MAX_EPOCHS 50
+#define MAX_EPOCHS 20
 
 // these are all the global variables used by the program
 
 // users and movies are one indexed
-const double numUsers = 458293;
-const double numMovies = 17770;
-const int numPts = 102416306;
+const int num_users = 458293;
+const int num_movies = 17770;
+const int num_pts = 102416306;
+const double lambda = 0.05;
 
 // K is the constant representing the number of features
 // lrate is the learning rate
-const double K = 50;
+const int K = 50;
 const double lrate = 0.01;
-const double lambda = 0.05;
 const double global_mean = 3.6033;
 
 // these 2D arrays are the U and V in the SVD
-double **userValues;
-double **movieValues;
+// they are one dimensional arrays in memory to make access quicker
+double *user_values;
+int user_values_size = (num_users + 1) * K;
+double *movie_values;
+int movie_values_size = (num_movies + 1) * K;
 
-// 2D arrays to store ratings and indexes
-double **ratings;
-double *indexes;
+// 2D arrays to store ratings input
+// again, it's a one dimensional array in memory to make access quicker
+double *ratings;
+int ratings_size = (int) (num_pts * POINT_SIZE);
+// stores the indices of each point, in the same order as the ratings
+double *indices;
 
 // arrays for user and movie biases
-double *userBiases;
-double *movieBiases;
+double *user_biases;
+int user_biases_size = num_users + 1;
+double *movie_biases;
+int movie_biases_size = num_movies + 1;
 
-double getRandom() {
+
+inline double getRandom() {
   // generate random number from 10 to 100
    int first = rand() % 91 + 10;
    double second = 1.0;
@@ -47,57 +58,64 @@ double getRandom() {
 }
 
 // creates arrays for storage
-void initialize()
+inline void initialize()
 {
-      cout << "Initializing the program.\n";
-      // allocate and initialize the userValues and movieValues matrices
-      // all of the +1 terms result from the fact that these arrays are 1
-      // indexed in the data
-      userValues = new double*[((int) numUsers + 1)];
-      for (int i = 0; i < numUsers + 1; i++)
-      {
-        userValues[i] = new double[(int) K];
-        for (int j = 0; j < K; j++)
-        {
-          userValues[i][j] = 0.1 * (double)(rand() % 10) + 0.01; // arbitrary initial condition
-        }
-      }
+    cout << "Initializing the program.\n";
 
-      movieValues = new double*[((int) numMovies + 1)];
-      for (int i = 0; i < numMovies + 1; i++)
-      {
-        movieValues[i] = new double[(int) K];
-        for (int j = 0; j < K; j++)
-        {
-          movieValues[i][j] = 0.1 * (double)(rand() % 10) + 0.01; // arbitrary initial condition
-        }
-      }
-      // create  the arrays that store the ratings input data and the indexes
-      ratings = new double* [((int) numPts)];
-      for(int i = 0; i < numPts; i++)
-      {
-          ratings[i] = new double [4];
-      }
+    // allocate and initialize the user_values and movie_values matrices
+    // all of the +1 terms result from the fact that these arrays are 1
+    // indexed in the data
+    user_values = new double[user_values_size];
+    for (int i = 0; i < user_values_size; i++)
+    {
+        user_values[i] = 0.1 * (double)(rand() % 10) + 0.01; // arbitrary initial condition
+    }
 
-      indexes = new double [((int) numPts)];
+    movie_values = new double[movie_values_size];
+    for (int i = 0; i < movie_values_size; i++)
+    {
+        movie_values[i] = 0.1 * (double)(rand() % 10) + 0.01;
+    }
 
-      userBiases = new double [((int) numUsers + 1)];
-      for (int i = 0; i < numUsers + 1; ++i)
-      {
-          userBiases[i] = 0.1 * (double)(rand() % 10) + 0.01; // arbitrary initial condition
-      }
+    // create  the arrays that store the ratings input data and the indexes
+    ratings = new double[ratings_size];
+    indices = new double[num_pts];
 
-      movieBiases = new double [((int) numMovies + 1)];
-      for (int i = 0; i < numMovies + 1; ++i)
-      {
-          movieBiases[i] = 0.1 * (double)(rand() % 10) + 0.01; // arbitrary initial condition
-      }
+    user_biases = new double [user_biases_size];
+    for (int i = 0; i < user_biases_size; ++i)
+    {
+        user_biases[i] = 0.1 * (double)(rand() % 10) + 0.01; // arbitrary initial condition
+    }
 
-      cout << "Done allocating memory.\n";
+    movie_biases = new double [movie_biases_size];
+    for (int i = 0; i < movie_biases_size; ++i)
+    {
+        movie_biases[i] = 0.1 * (double)(rand() % 10) + 0.01; // arbitrary initial condition
+    }
+
+    cout << "Done allocating memory.\n";
+}
+
+/*
+* Reads the input data into ratings and indices
+*/
+inline void read_data()
+{
+    cout << "Reading in training data.\n";
+
+    // read in ratings data - currently, this is training without the baseline
+    fstream ratings_file("ratings_baseline_removed.bin", ios::in | ios::binary);
+    ratings_file.read((char *)(ratings), sizeof(double) * num_pts * POINT_SIZE);
+    ratings_file.close();
+
+    // read in index data
+    fstream indices_file("indices.bin", ios::in | ios::binary);
+    indices_file.read((char *)(indices), sizeof(double) * num_pts);
+    indices_file.close();
 }
 
 // reads in and stores the ratings and indexes
-void readRatingsIndexes()
+inline void readRatingsIndexes()
 {
     cout << "Reading in the training data." << "\n";
     cout << "Note to user: Make sure the input file paths are correct.\n";
@@ -113,18 +131,18 @@ void readRatingsIndexes()
     if (ratingsFile.is_open() && indexFile.is_open()) {
       while (ratingsFile >> inputs[0] >> inputs[1] >> inputs[2] >> inputs[3]) {
           indexFile >> index;
-          indexes[pt] = index;
+          indices[pt] = index;
 
           double user = inputs[0];
           double movie = inputs[1];
           double date = inputs[2];
           double rating = inputs[3];
 
-          ratings[pt][0] = user;
-          ratings[pt][1] = movie;
-          ratings[pt][2] = date;
+          ratings[pt * POINT_SIZE] = user;
+          ratings[pt * POINT_SIZE + 1] = movie;
+          ratings[pt * POINT_SIZE + 2] = date;
           // subtract baseline from ratings
-          ratings[pt][3] = rating - baselinePrediction(user, movie, date);
+          ratings[pt * POINT_SIZE + 3] = rating - baselinePrediction(user, movie, date);
 
           // increment pt so that we update the values for a new point next time
           pt += 1;
@@ -133,20 +151,20 @@ void readRatingsIndexes()
 
     ratingsFile.close();
     indexFile.close();
-}
+} 
 
 // given a user and a movie this function gives the predicted rating
-double predictRating(int user, int movie)
+inline double predict_rating(int user, int movie)
 {
     double rating = 0;
     for (int i = 0; i < K; i++)
     {
-        rating += userValues[user][i] * movieValues[movie][i];
+        rating += user_values[user * K + i] * movie_values[movie * K + i];
     }
     return rating;
 }
 
-double magSquared(double* vector) {
+inline double magSquared(double* vector) {
     double mag = 0;
     for (int i = 0; i < K; i++) {
         mag += vector[i] * vector[i];
@@ -158,116 +176,128 @@ double magSquared(double* vector) {
 * Gets the total error of the SVD model on the set index provided
 * i.e., to get validation error, pass in set = 2
 */
-double error (int set)
+inline double error(int set)
 {
-  cout << "Calculating Validation error..." << "\n";
+    cout << "Calculating error.\n";
 
-  //  counter keeps track of the number of points we've been through
-  int counter = 0;
-  double error = 0;
-  double diff = 0;
-  int index = 0;
-  double numValidationPts = 0;
+    double error = 0;
+    double diff = 0;
+    double index;
+    double points_in_set = 0;
 
-  for (int i = 0; i < numPts; i++) {
-      index = indexes[i];
+    for (int i = 0; i < num_pts; i++) {
+        index = indices[i];
+        if (index == set) {
+            int user = (int)ratings[i * POINT_SIZE];
+            int movie = (int)ratings[i * POINT_SIZE + 1];
+            double rating = ratings[i * POINT_SIZE + 3];
+            double prediction = user_biases[user] + movie_biases[movie] + predict_rating(user, movie);
+            //cout << "prediction: " << prediction << "\n";
+            //cout << "rating: " << rating << "\n";
 
-      if (index == set) {
-          int user = ratings[i][0];
-          int movie = ratings[i][1];
-          double rating = ratings[i][3];
-          diff = rating - (userBiases[user] + movieBiases[movie] + predictRating(user, movie));
-          //cout << diff << "\n";
+            diff = rating - prediction;
+            error += diff * diff;
 
-          double errorpart1 = diff * diff;
-          //double errorpart2 = (magSquared(userValues[user]) + magSquared(movieValues[movie]) + 
-            //userBiases[user] * userBiases[user] + movieBiases[movie] * movieBiases[movie]);
-          //double errorpart2 = magSquared(userValues[user]) + magSquared(movieValues[movie]);
+            //cout << "diff^2: " << diff * diff << "\n";
+            //cout << "error: " << error << "\n";
+            //cout << "pt: " << i << "\n";
+            double inf = std::numeric_limits<double>::infinity();
 
-          error += errorpart1;
-          numValidationPts += 1;
-      }
-      counter++;
-  }
-
-  return sqrt(error/numValidationPts);
-}
-
-void train(int user, int movie, int rating, int feature)
-{
-    // calculate the error with the current feature values
-    double err = (double) rating - (userBiases[user] + movieBiases[movie] + predictRating(user, movie));
-
-    // updates the movie and user vectors for given feature
-    double *uv = userValues[user];
-
-    userValues[user][feature] += lrate * (err * movieValues[movie][feature] - lambda * userValues[user][feature]);
-    movieValues[movie][feature] += lrate * (err * uv[feature] - lambda * movieValues[movie][feature]);
-
-    // update biases
-    double oldUserBias = userBiases[user];
-    userBiases[user] += lrate * (err - lambda * (oldUserBias + movieBiases[movie] - global_mean));
-    movieBiases[movie] += lrate * (err - lambda * (oldUserBias + movieBiases[movie] - global_mean));
-}
-
-void runEpoch (int feature)
-{
-    cout << "Running Epoch..." << "\n";
-    int pt;
-    int index;
-
-    for (int i = 0; i < numPts; i++) {
-        // select a random pt to train with
-        pt = rand() % numPts;
-        index = indexes[pt];
-
-        // make sure the selected point is in the first data set
-        while (index != 1)
-        {
-            pt = rand() % numPts;
-            index = indexes[pt];
-        }
-
-        // train on this point
-        train(ratings[pt][0], ratings[pt][1], ratings[pt][3], feature);
-    }
-    cout << "Epoch complete." << "\n";
-}
-
-void findQualPredictions()
-{
-    cout << "Finding Qual Predictions" << "\n";
-
-    ofstream outputFile;
-    outputFile.open("output.dta");
-    int index;
-    
-    for (int i = 0; i < numPts; i++) {
-        index = indexes[i];
-
-        if(index == 5){
-          int user = ratings[i][0];
-          int movie = ratings[i][1];
-          double date = ratings[i][2];
-
-           // add back baseline into predictions
-          double prediction = (userBiases[user] + movieBiases[movie] + 
-              predictRating(user, movie)) + baselinePrediction(user, movie, date);
-
-           // clip predictions within range of 1 and 5
-           if(prediction < 1){
-              prediction = 1;
-           }
-           if(prediction > 5){
-              prediction = 5;
+            if(error >= inf){
+              cout << "ERROR IS INFINITY!!" << "\n";
+              return 1000000;
             }
 
-           outputFile << prediction << "\n";
+            points_in_set += 1;
         }
     }
+
+    return sqrt(error/points_in_set);
 }
 
-void storeUserFeatures()
+inline void train(int user, int movie, double rating, int feature)
+{
+    // calculate the error with the current feature values
+    double err = rating - (user_biases[user] + movie_biases[movie] + predict_rating(user, movie));
+
+    // updates the movie and user vectors for given feature
+    double uv = user_values[user * K + feature];
+
+    user_values[user * K + feature] += lrate * (err * movie_values[movie * K + feature] - lambda * uv);
+    movie_values[movie * K + feature] += lrate * (err * uv - lambda * movie_values[movie * K + feature]);
+
+    // update biases
+    double oldUserBias = user_biases[user];
+    user_biases[user] += lrate * (err - lambda * (oldUserBias + movie_biases[movie] - global_mean));
+    movie_biases[movie] += lrate * (err - lambda * (oldUserBias + movie_biases[movie] - global_mean));
+}
+
+/*
+* Run a full epoch.
+*/
+inline void run_epoch (int feature)
+{
+  cout << "Running Epoch." << "\n";
+
+  int pt;
+  double index;
+
+  for (int i = 0; i < num_pts; i++) {
+     // select an arbitrary point to train with 
+    pt = rand() % num_pts;
+    index = indices[pt];
+      // make sure the selected point is in the first data set
+      while (index != 1)
+      {
+          pt = rand() % num_pts;
+          index = indices[pt];
+      }
+      // train with this point
+    int user = (int)ratings[pt * POINT_SIZE];
+    int movie = (int)ratings[pt * POINT_SIZE + 1];
+    double rating = ratings[pt * POINT_SIZE + 3];
+    train(user, movie, rating, feature);
+  }
+
+  cout << "Epoch complete." << "\n";
+}
+
+inline void find_qual_predictions()
+{
+  ofstream outputFile;
+  outputFile.open("SVD_bias_output.dta");
+
+  double index;
+  double prediction;
+
+  int user;
+  int movie;
+  int date;
+  for(int i = 0; i < num_pts; i++)
+  {
+      index = indices[i];
+      // the qual set is set 5
+      if (index == 5)
+      {
+        user = (int)ratings[i * POINT_SIZE];
+        movie = (int)ratings[i * POINT_SIZE + 1];
+        date = (int)ratings[i * POINT_SIZE + 2];
+          prediction = baselinePrediction(user, movie, date) + predict_rating(user, movie)
+              + user_biases[user] + movie_biases[movie];
+          if (prediction < 1)
+          {
+              prediction = 1;
+          }
+          if (prediction > 5)
+          {
+              prediction = 5;
+          }
+          outputFile << prediction << "\n";
+      }
+  }
+}
+
+inline void storeUserFeatures()
 {
     cout << "storing user features" << "\n";
 
@@ -275,126 +305,63 @@ void storeUserFeatures()
     featureFile.open("userFeatures.dta");
     // file has userid followed by all K feature values
 
-    for (int user = 0; user < numUsers; user++) {
+    for (int user = 0; user < num_users; user++) {
        featureFile << user << " ";
        for (int feature = 0; feature < K; feature++) {
-           featureFile << userValues[user][feature] << " ";
+           featureFile << user_values[user * K + feature] << " ";
        }
        featureFile << "\n";
     }
 }
 
-void cleanUp()
+/*
+* Clears all used memory
+*/
+inline void clean_up()
 {
-      cout << "Cleaning up\n" << "\n";
-      // de-allocate user values and movie values to prevent memory leak
-      for (int i = 0; i < numUsers + 1; i++)
-      {
-        delete [] userValues[i];
-      }
-      delete [] userValues;
-      for (int i = 0; i < numMovies + 1; i++)
-      {
-        delete [] movieValues[i];
-      }
-      delete [] movieValues;
-
-      for (int i = 0; i < numPts; i++) {
-        delete[] ratings[i];
-      }
-      delete[] ratings;
-
-      delete[] indexes;
-
-      delete[] userBiases;
-
-      delete[] movieBiases;
+    cout << "Cleaning up.\n";
+    delete [] user_values;
+    delete [] movie_values;
+    delete [] ratings;
+    delete [] indices;
+    delete [] user_biases;
+    delete [] movie_biases;
 }
 
-// Opens the file and runs the SVD
 int main()
 {
     initialize();
     readRatingsIndexes();
-    // gets the initial validation error
-    // NOTE: the initial error should calculate the error in the final program
+
     double initialError = 10000;
-    double finalError = error(2);
-    int epochCounter = 0;
-    int featureEpochCounter = 0;
+    double finalError = error(2); // gets the validation error before training
+    int counter = 1;
 
-    cout << "Initial Error is: " << finalError << "\n";
+    cout << "The starting error is: " << finalError << "\n";
 
-    for(int i = 0; i < K; i++) {
+    for (int feature = 0; feature < K; feature++) {
+      int featureEpochCounter = 1;
       initialError = 10000;
-      featureEpochCounter = 0;
+
       while (initialError - finalError > STOPPING_CONDITION && featureEpochCounter <= MAX_EPOCHS) {
-          cout << "Feature " << i << "\n";
-          cout << "Starting Epoch " << epochCounter  << "\n";
-          initialError = finalError;
-          runEpoch(i);
-          finalError = error(2); // error(2) returns the validation error
-          cout << "Error after Epoch " << epochCounter  << ": " << finalError << "\n";
-          featureEpochCounter++;
-          epochCounter++;
-          cout << "-----------------------------------\n";
+        cout << "Feature: " << feature << "\n";
+        cout << "Starting Epoch " << counter << "\n";
+
+        initialError = finalError;
+        run_epoch(feature);
+        finalError = error(2); // error(2) returns the validation error
+
+        cout << "Error after Epoch " << counter << ": " << finalError << "\n";
+        counter++;
+        featureEpochCounter++;
+        cout << "-----------------------------------\n";
       }
-  }
+    }
+    
+    // find the values on the qual set
+    find_qual_predictions();
 
-    // train one feature at a time
-   //for(int i = 0; i < K; i++) {
-      //initialError = 10;
-      //featureEpochCounter = 1;
-        // while error is decreasing by threshold
-        /*while (initialError - finalError > threshold) {
-          cout << "Feature " << i << "\n";
-          cout << "Starting Epoch " << epochCounter << "\n";
-
-          epochCounter++;
-          featureEpochCounter++;
-
-          initialError = finalError;
-          runEpoch(i);
-          finalError = error();
-
-          cout << "Error after Epoch " << finalError << "\n";
-       }
-       int unforcedEpochs = featureEpochCounter;
-       // didn't train on feature, because initialError - finalError < 0.0001 already
-       // just train for n more epochs
-       int minEpochs = 10;
-       if (unforcedEpochs < minEpochs) {
-          for (int j = 0; j < minEpochs - unforcedEpochs; j++) {
-            cout << "FORCE TRAINING" << "\n";
-            cout << "Feature " << i << "\n";
-            cout << "Starting Epoch " << epochCounter << "\n";
-
-            epochCounter++;
-            featureEpochCounter++;
-
-            initialError = finalError;
-            runEpoch(i);
-            finalError = error();
-
-            cout << "Error after Epoch " << finalError << "\n";
-          }
-        }*/
-      /*  for(int j = 0; j < 30; j++){
-          cout << "Feature " << i << "\n";
-          cout << "Starting Epoch " << j << "\n";
-
-          initialError = finalError;
-          runEpoch(i);
-          finalError = error();
-
-          cout << "Error after Epoch " << finalError << "\n";
-
-          featureEpochCounter++;
-        }*/
-      findQualPredictions();
-      storeUserFeatures();
-      cleanUp();
-      return 0;
-  }
-
-//}
+    clean_up();
+    return 0;
+}
+ 
